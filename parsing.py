79a94d70f7088aa4b44604037ab5eb16f90eec3b
Jie Ying Huang <124211458+jieyingh@users.noreply.github.com>
2024-03-01 14:09:58 -0500

    argparse
    created argparse function for program

---
+++ parsing.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+
+def file_path(path):
+    if os.path.isfile(path):
+        return path
+    else:
+        raise FileNotFoundError(path)
+
+def parse():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('x', type=file_path,
+                        help='Sequences to be classified')
+    parser.add_argument('y', type=file_path,
+                        help='Class of sequences')
+    parser.add_argument('-k', '--kmer', default=6, type=int,
+                        help='Number of k-mers to generate')
+    parser.add_argument('-m', '--method', default='all', choices=['knn', 'dt', 'svc', 'mnb', 'all'],
+                        help='Method to use for classification')
+    parser.add_argument('-s', '--show', action='store_true', default=True,
+                        help='Show plots')   
+    parser.add_argument('-v', '--verbose', action='store_true', default=True,
+                        help='adds verbosity')
+
+    args = parser.parse_args()
+
+    return args
+
+# args = parse()
+# print(args)
